58aee712d524db46ed95d49a8de9c948240cee15
pereza <alejandro.perez@socialbakers.com>
2023-04-06 10:00:11 +0200

    second commit

---
+++ test/test_fancy_python_function.py
@@ -0,0 +1,8 @@
+import pytest
+
+from my_python_code.fancy_python_code import print_hello
+
+
+@pytest.mark.parametrize('name', ['', 'Hello, world!', 'Testing 123'])
+def test_print_hello(name):
+    assert print_hello(name) is None
