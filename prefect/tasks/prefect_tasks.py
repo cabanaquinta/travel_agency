55e38ff09d5b42c820ab8900babfc924ee01ac13
github.com-user1 <alejandro.perez@socialbakers.com>
2023-08-07 21:10:33 +0200

    new_commit

--- prefect/tasks/prefect_tasks.py
+++ prefect/tasks/prefect_tasks.py
@@ -111,7 +111,6 @@ def crawl(spider) -> None:
 
 
 # Web to Bucket
-
 @task(retries=3)
 def fetch() -> dict:
     """ Fetch the data from the URL """
@@ -151,7 +150,6 @@ def upload_to_gcs(path: Path) -> None:
 
 
 # Bucket to BigQuery
-
 @task(retries=3)
 def upload_to_bq(path: Path, method: str = 'append') -> None:
     """ Read files from Bucket and paste them to BigQuery """
