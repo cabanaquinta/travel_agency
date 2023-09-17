6c716d20a067f8717b4dd35a4f11d1f7ad283260
Alejandro Perez <alexroperez4@gmail.com>
2023-09-17 12:41:12 +0000

    nn

--- dbt/models/staging/web_data/stg_web-data__master.sql
+++ dbt/models/staging/web_data/stg_web-data__master.sql
@@ -6,7 +6,16 @@ with
         from {{ source("staging", "master") }}
     )
 select
-    *,
+    link,
+    description,
+    price,
+    currency,
+    start as start_city,
+    unique_ids.end as end_city,
+    date_from,
+    date_to,
+    collected_date,
+    DATE_TRUNC(collected_date, DAY) as collected_day,
      {{ dbt_utils.surrogate_key(["description", "collected_date", "link"]) }} as trip_id
 from unique_ids
 where rn = 1
